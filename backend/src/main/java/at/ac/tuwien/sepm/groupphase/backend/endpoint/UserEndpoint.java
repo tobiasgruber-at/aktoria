7a3a1466e3d2faef503e70ed33e5485a466d30f6
Julia Bernold <julia.bernold@tuwien.ac.at>
2022-05-05 21:29:01 +0200

    #19: commit test

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
@@ -6,7 +6,16 @@ import org.h2.engine.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -15,7 +24,7 @@ public class UserEndpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(User.class);
 
     public UserEndpoint() {
-        //TODO: implement constructor
+        //TODO: implement constructor ---
     }
 
     @PostMapping
@@ -25,6 +34,7 @@ public class UserEndpoint {
         LOGGER.info("POST /api/v1/users");
 //TODO: write tests and implement method
         return null;
+
     }
 
     @PutMapping(path = "/{id}")
