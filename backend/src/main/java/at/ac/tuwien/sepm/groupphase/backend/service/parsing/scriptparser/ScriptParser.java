fa012602a43c307af741eb4672244910c28b3e35
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-05-05 16:16:02 +0200

    #21: reformat code to conform with checkstyle

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/parsing/scriptparser/ScriptParser.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/parsing/scriptparser/ScriptParser.java
@@ -9,15 +9,15 @@ public interface ScriptParser {
     /**
      * Parses the script.
      *
-     * Parses the script into pages, roles and lines. For a correct result
+     * <p>Parses the script into pages, roles and lines. For a correct result
      * the given input has to be conformed to a given format standard.
      *
-     * Role declarations have to be written in uppercase. The line that is spoken
+     * <p>Role declarations have to be written in uppercase. The line that is spoken
      * by the given role has to follow the role declaration.
      *
-     * Lines that are not leaded by a role declaration are given no role.
+     * <p>Lines that are not leaded by a role declaration are given no role.
      *
-     * A page contains all lines, that start on this page.
+     * <p>A page contains all lines, that start on this page.
      *
      * @return a new instance of ParsedScript
      */
