9ff41daf667bfffdc3dcbc00be9c8cbf4b1b1002
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-05-07 21:04:24 +0200

    #21: update endpoint

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ScriptEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ScriptEndpoint.java
@@ -3,6 +3,8 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.StagedScriptDto;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
 import at.ac.tuwien.sepm.groupphase.backend.service.ScriptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,20 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
+import java.lang.invoke.MethodHandles;
 
 @RestController
-@RequestMapping(path = "/api/v1/scripts")
+@RequestMapping(path = ScriptEndpoint.path)
 public class ScriptEndpoint {
-
+    public static final String path = "/api/v1/scripts";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final ScriptService scriptService;
 
     public ScriptEndpoint(ScriptService scriptService) {
         this.scriptService = scriptService;
     }
 
-    @PostMapping
+    @PostMapping(path = "/upload")
     @ResponseStatus(HttpStatus.OK)
     public StagedScriptDto uploadScript(@RequestBody File file) {
+        LOGGER.info("POST {}/upload", path);
+
         try {
             return scriptService.newScript(file);
         } catch (ServiceException e) {
