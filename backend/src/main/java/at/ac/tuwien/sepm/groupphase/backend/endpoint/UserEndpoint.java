fd6cef0db1be438ce337062cf341161034c015f0
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-09 22:25:26 +0200

    #19: refactor with lombok

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
@@ -8,8 +8,7 @@ import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
 import at.ac.tuwien.sepm.groupphase.backend.exception.UserNotFoundException;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepm.groupphase.backend.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +23,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.lang.invoke.MethodHandles;
-
 /**
  * Endpoint for user related requests.
  *
@@ -33,9 +30,9 @@ import java.lang.invoke.MethodHandles;
  */
 @RestController
 @RequestMapping(path = UserEndpoint.path)
+@Slf4j
 public class UserEndpoint {
     public static final String path = "/api/v1/users";
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final UserService userService;
 
     public UserEndpoint(UserService userService) {
@@ -46,7 +43,7 @@ public class UserEndpoint {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public SimpleUserDto getUser(@PathVariable Long id) throws ServiceException {
-        LOGGER.info("GET " + UserEndpoint.path);
+        log.info("GET " + UserEndpoint.path);
         return userService.getUser(id);
     }
 
@@ -54,11 +51,11 @@ public class UserEndpoint {
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public UserRegistrationDto postUser(@RequestBody UserRegistrationDto userRegistrationDto) throws ServiceException {
-        LOGGER.info("POST " + UserEndpoint.path);
+        log.info("POST " + UserEndpoint.path);
         try {
             return userService.createUser(userRegistrationDto);
         } catch (ValidationException e) {
-            LOGGER.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage(), e);
         }
     }
@@ -67,7 +64,7 @@ public class UserEndpoint {
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @ResponseBody
     public DetailedUserDto patchUser(@RequestParam Boolean passwordChange, @RequestBody UpdateUserDto updateUserDto, @PathVariable Long id) throws ServiceException {
-        LOGGER.info("PATCH " + UserEndpoint.path + "/{}", id);
+        log.info("PATCH " + UserEndpoint.path + "/{}", id);
         //this method calls either changeUserData or changePassword or both
         // TODO: create path method in Service
         // return userService.patch(updateUserDto, passwordChange, id);
@@ -77,13 +74,13 @@ public class UserEndpoint {
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     public void deleteUser(@PathVariable Long id) throws ServiceException {
-        LOGGER.info("DELETE {}/{}", UserEndpoint.path, id);
+        log.info("DELETE {}/{}", UserEndpoint.path, id);
         //  try {
         userService.deleteUser(id);
         //  }
         /*TODO: uncomment as soon as Service is implemented
         catch(UserNotFoundException e){
-            LOGGER.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }*/
     }
@@ -91,11 +88,11 @@ public class UserEndpoint {
     @PostMapping(path = "/forgotten-password")
     @ResponseStatus(HttpStatus.CREATED)
     public void forgottenPassword(@RequestBody String email) {
-        LOGGER.info("POST " + UserEndpoint.path + "/forgotten-password");
+        log.info("POST " + UserEndpoint.path + "/forgotten-password");
         try {
             userService.forgotPassword(email);
         } catch (UserNotFoundException e) {
-            LOGGER.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
     }
