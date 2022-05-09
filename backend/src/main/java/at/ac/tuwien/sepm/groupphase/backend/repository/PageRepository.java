e32b3bffa0db7c6adea17e502fe984d38523405b
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-09 20:09:29 +0200

    #19: move repository package

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PageRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/PageRepository.java
@@ -1,4 +1,4 @@
-package at.ac.tuwien.sepm.groupphase.backend.security.repository;
+package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Page;
 import at.ac.tuwien.sepm.groupphase.backend.entity.id.PageId;
