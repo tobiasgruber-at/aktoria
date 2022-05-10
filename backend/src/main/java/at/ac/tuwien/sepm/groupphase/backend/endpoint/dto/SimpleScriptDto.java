96c3d04dd931a245415d27c0016d2a972255d4cd
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-05-10 04:00:51 +0200

    #21: remove deprecated static method

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SimpleScriptDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SimpleScriptDto.java
@@ -1,12 +1,8 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.PageMapper;
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.RoleMapper;
-import at.ac.tuwien.sepm.groupphase.backend.service.parsing.script.ParsedScript;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -30,11 +26,4 @@ public class SimpleScriptDto {
     private String name;
     private List<SimplePageDto> pages;
     private List<SimpleRoleDto> roles;
-
-    @Deprecated
-    public static SimpleScriptDto of(ParsedScript parsedScript, String name) {
-        List<SimpleRoleDto> roles = Mappers.getMapper(RoleMapper.class).listOfStringToListOfSimpleRoleDto(parsedScript.getRoles());
-        List<SimplePageDto> pages = Mappers.getMapper(PageMapper.class).listOfPageToListOfSimplePageDto(parsedScript.getPages());
-        return new SimpleScriptDto(name, pages, roles);
-    }
 }
