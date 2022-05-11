0c56088f2dd2d4888f4283fbaaea99023cb46361
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-11 13:33:04 +0200

    #19: adapt registration form to endpoint (ui)

--- frontend/src/app/modules/core/services/user/user-mock.service.ts
+++ frontend/src/app/modules/core/services/user/user-mock.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import {
-  AuthenticatedUser,
-  UserRegistration
-} from '../../../shared/dtos/user-dtos';
+import { SimpleUser, UserRegistration } from '../../../shared/dtos/user-dtos';
 import { UserService } from './user-service';
 import { randomDelay } from '../../../shared/functions/random-delay';
-import { tap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth-service';
 
 @Injectable()
@@ -21,12 +17,9 @@ export class UserMockService extends UserService {
     return null;
   }
 
-  register(user: UserRegistration): Observable<AuthenticatedUser> {
+  register(user: UserRegistration): Observable<SimpleUser> {
     return of(
-      new AuthenticatedUser(1, user.name, user.email, false, this.mockedToken)
-    ).pipe(
-      randomDelay(),
-      tap((res: AuthenticatedUser) => this.authService.setToken(res.jwtToken))
+      new SimpleUser(1, user.firstName, user.lastName, user.email, false)
     );
   }
 
