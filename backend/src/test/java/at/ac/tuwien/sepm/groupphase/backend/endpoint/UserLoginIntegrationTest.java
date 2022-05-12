5e04947f804020a1099beb0df90316053c41ecd0
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-05-12 23:09:56 +0200

    #21: wip login integration test

---
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserLoginIntegrationTest.java
@@ -0,0 +1,86 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint;
+
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SimpleUserDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserRegistrationDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles({ "test", "datagen" })
+@SpringBootTest
+@EnableWebMvc
+@WebAppConfiguration
+public class UserLoginIntegrationTest {
+
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+        .withConfiguration(GreenMailConfiguration.aConfig().withUser("tester", "password"))
+        .withPerMethodLifecycle(true);
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("login works correctly")
+    void login() throws Exception {
+        byte[] body = mockMvc
+            .perform(MockMvcRequestBuilders
+                .post("/api/v1/users")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserRegistrationDto("Varok", "Saurfang", "varok.saurfang@email.com", "forthehorde")))
+                .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isCreated())
+            .andReturn().getResponse().getContentAsByteArray();
+
+        final SimpleUserDto response = objectMapper.readValue(body, SimpleUserDto.class);
+        final SimpleUserDto expected = new SimpleUserDto(response.getId(), "Varok", "Saurfang", "varok.saurfang@email.com", false);
+
+        assertNotNull(response);
+        assertEquals(response, expected);
+
+        body = mockMvc
+            .perform(MockMvcRequestBuilders
+                .post("/api/v1/authentication")
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{"
+                    + "\"email\": \"varok.saurfang@email.com\","
+                    + "\"password\": \"forthehorde\""
+                    + "}")
+                .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsByteArray();
+
+        final String token = objectMapper.readValue(body, String.class);
+        assertTrue(token.startsWith("Bearer "));
+    }
+}
