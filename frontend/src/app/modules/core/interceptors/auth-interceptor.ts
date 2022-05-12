83c5a99aa51b20d42e8a660b27f3457751be6752
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-13 00:24:16 +0200

    #19: complete email verification

--- frontend/src/app/modules/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/modules/core/interceptors/auth-interceptor.ts
@@ -44,7 +44,6 @@ export class AuthInterceptor implements HttpInterceptor {
       blacklistedEndpoints.push('/users');
       blacklistedEndpoints.push('/users/verification');
     }
-
     return blacklistedEndpoints.some(
       (e) => req.url === this.globals.backendUri + e
     );
