07396257d222df693d9ea89f373256c33f8068a9
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-15 15:59:23 +0200

    #21: add get script endpoint

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ScriptService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ScriptService.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ScriptDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ScriptPreviewDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SimpleScriptDto;
 import at.ac.tuwien.sepm.groupphase.backend.exception.IllegalFileFormatException;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -57,5 +58,5 @@ public interface ScriptService {
      * @return the script corresponding to the given id.
      * @throws ServiceException when an error occurs while trying to get the script
      */
-    ScriptDto findById(Long id) throws ServiceException;
+    ScriptDto findById(Long id) throws ServiceException, NotFoundException;
 }
