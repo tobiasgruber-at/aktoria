f34513674486a5e13ea4c654b410e120e14d3af3
tobiasgruber <e11912367@student.tuwien.ac.at>
2022-05-16 00:47:23 +0200

    #26: add page transitions

---
+++ frontend/src/app/modules/shared/animations/fixed-appear-animations.ts
@@ -0,0 +1,30 @@
+import {
+  animate,
+  AnimationTriggerMetadata,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+const voidStyles: { [property: string]: string | number } = {
+  opacity: 0,
+  transform: 'translateY(20px)'
+};
+const transitionDuration = 550;
+const transitionCurve = 'cubic-bezier(.42, 0, .09, 1)';
+const transitionDelay = 0;
+
+export const fixedAppearAnimations: AnimationTriggerMetadata = trigger(
+  'fixedAppear',
+  [
+    state('void', style(voidStyles)),
+
+    transition(':enter', [
+      animate(`${transitionDuration}ms ${transitionDelay}ms ${transitionCurve}`)
+    ]),
+    transition(':leave', [
+      animate(`${transitionDuration}ms ${transitionCurve}`, style(voidStyles))
+    ])
+  ]
+);
