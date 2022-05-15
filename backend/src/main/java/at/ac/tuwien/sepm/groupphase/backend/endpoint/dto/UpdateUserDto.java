0807449138750ea4e752b017c4c53663f90db852
Luke Nemeskeri <e11925203@studen.tuwien.ac.at>
2022-05-15 18:47:32 +0200

    #19: changed extends from DetailedUserDto and UpdateUserDto

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/UpdateUserDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/UpdateUserDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UpdateUserDto extends DetailedUserDto {
+public class UpdateUserDto {
     private Long id;
     private String firstName;
     private String lastName;
