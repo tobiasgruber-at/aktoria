04ce6365cad12773c041726cc5bd1fa2ba13027b
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-05-16 17:46:39 +0200

    #16: update rest endpoints

--- frontend/src/app/modules/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/modules/core/interceptors/auth-interceptor.ts
@@ -1,9 +1,14 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Globals} from '../global/globals';
-import {AuthService} from '../services/auth/auth-service';
-import {UserService} from '../services/user/user-service';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Globals } from '../global/globals';
+import { AuthService } from '../services/auth/auth-service';
+import { UserService } from '../services/user/user-service';
 
 /** @author Tobias Gruber */
 @Injectable()
@@ -12,8 +17,7 @@ export class AuthInterceptor implements HttpInterceptor {
     private authService: AuthService,
     private userService: UserService,
     private globals: Globals
-  ) {
-  }
+  ) {}
 
   /** Intercepts outgoing http requests and adds authorizations headers if necessary. */
   intercept(
@@ -44,9 +48,9 @@ export class AuthInterceptor implements HttpInterceptor {
     } else if (req.method === 'POST') {
       blacklistedEndpoints.push('/users');
       blacklistedEndpoints.push('/users/verification');
-      blacklistedEndpoints.push('/users/reset-password');
+      blacklistedEndpoints.push('/users/forgot-password');
     } else if (req.method === 'PUT') {
-      blacklistedEndpoints.push('/users/change-password');
+      blacklistedEndpoints.push('/users/reset-password');
     }
     return blacklistedEndpoints.some(
       (e) => req.url === this.globals.backendUri + e
