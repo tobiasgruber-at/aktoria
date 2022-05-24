516c36e5ab0bd1061a58922533a764ae7b6e1f9c
Marvin Flandorfer <e52004069@student.tuwien.ac.at>
2022-05-24 18:24:56 +0200

    #35: unfinished save implementation

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/SessionServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/SessionServiceImpl.java
@@ -2,10 +2,16 @@ package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SessionDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SimpleSessionDto;
+import at.ac.tuwien.sepm.groupphase.backend.entity.User;
+import at.ac.tuwien.sepm.groupphase.backend.exception.UnauthorizedException;
+import at.ac.tuwien.sepm.groupphase.backend.repository.RoleRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.SectionRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.SessionRepository;
+import at.ac.tuwien.sepm.groupphase.backend.service.AuthorizationService;
 import at.ac.tuwien.sepm.groupphase.backend.service.SessionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Specific implementation of SessionService
@@ -17,13 +23,26 @@ import org.springframework.stereotype.Service;
 public class SessionServiceImpl implements SessionService {
 
     private final SessionRepository sessionRepository;
+    private final SectionRepository sectionRepository;
+    private final RoleRepository roleRepository;
+    private final AuthorizationService authorizationService;
 
-    public SessionServiceImpl(SessionRepository sessionRepository) {
+    public SessionServiceImpl(SessionRepository sessionRepository, SectionRepository sectionRepository, RoleRepository roleRepository, AuthorizationService authorizationService) {
         this.sessionRepository = sessionRepository;
+        this.sectionRepository = sectionRepository;
+        this.roleRepository = roleRepository;
+        this.authorizationService = authorizationService;
     }
 
     @Override
+    @Transactional
     public SessionDto save(SimpleSessionDto simpleSessionDto) {
+        log.trace("save(sessionDto = {}", simpleSessionDto);
+        User user = authorizationService.getLoggedInUser();
+        if(user == null){
+            throw new UnauthorizedException();
+        }
+
         return null;
     }
 
