7260c932f3ff4186e7918dcfb4a2e6f5257bfaff
Marvin Flandorfer <e52004069@student.tuwien.ac.at>
2022-05-24 18:24:32 +0200

    #35: create new section repository

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/SectionRepository.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+}
