32d9074b4dc0ed68a7f3780d766ed3d7da206f14
Marvin Flandorfer <e52004069@student.tuwien.ac.at>
2022-05-27 00:11:33 +0200

    #28: removed unnecessary annotation

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ResetPasswordIntegrationTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ResetPasswordIntegrationTest.java
@@ -8,7 +8,6 @@ import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
